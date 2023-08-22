a0a7d1a3db852339e41a044fb28e36f3d2f9e163
Advait Naik <ad.naik0801@gmail.com>
2023-08-22 23:26:06 +0530

    Final Commit

--- Assignment_A3.py
+++ Assignment_A3.py
@@ -1,150 +1,149 @@
 import random
 
-def decimal_to_binary( x: int ) -> list[int]:
-    p = x
-    output: list[int] = []
-    while p != 0:
-        m = p // 2
-        output.append( p % 2 )
-        p = m
-    return output[::-1]
+def dec_to_bin(n):
+    b = bin(n)[2:]
+    return [int(i) for i in b]
 
-def binary_to_decimal( num: list[int] ) -> int:
-    output = 0
-    for i in range( len( num ) - 1 , -1 , -1 ):
-        output += 2**( len(num) - 1 - i ) * num[i]
-    return output
+def bin_to_dec(num):
+    num = [str(i) for i in num]
+    b = ''.join(num)
+    return int(b,2)
 
-def list_to_str( l: list[int] ) -> str:
-    return "".join( [ str(bit) for bit in l ] )
+def calculate_redundant_bits(message):
+    m = len(message)
+    for i in range(1,m):
+        if 2**i >= m + i + 1:
+            return i
 
-def get_even_parity( seq: list[int] ) -> int:
-    return int( sum(seq) % 2 != 0 )
+def even_parity(sequence):
+    total = sum(sequence)
+    if total % 2 != 0:
+        return int(True)
+    return int(False)
 
-def get_odd_parity( seq: list[int] ) -> int:
-    return int( sum(seq) % 2 == 0 )
+def odd_parity(sequence):
+    total = sum(sequence)
+    if total % 2 == 0:
+        return int(True)
+    return int(False)
 
-def encode( message: list[int] , parity: str ) -> tuple[ list[int] , int ]:
-    m = len( message )
+def sender(message,p):
+    redundant_bits = calculate_redundant_bits(message)
+    parity = even_parity if p == 'even' else odd_parity
+    code_word = [0 for i in range(len(message) + redundant_bits)]
+    parity_positions = [2**i - 1 for i in range(redundant_bits)]
+    data_positions = [i for i in range(len(message) + redundant_bits) if i not in parity_positions]
 
-    parity_func = get_even_parity if parity == "even" else get_odd_parity
+    for i,j in enumerate(data_positions):
+        code_word[j] = int(message[i])
 
-    # Calculate number of redundant bits
-    r = 0
-    while 2**r < m + r + 1:
-        r += 1
-    code_word = [ 0 for _ in range( m + r ) ]
-    parity_positions = [ 2**i - 1 for i in range(r) ]
-    data_positions = [ i for i in range( m + r ) if i not in parity_positions ]
-    
-    # Fill data bits in code_word
-    for i , pos in enumerate( data_positions ):
-        code_word[ pos ] = int(message[i])
-    
-    # Fill parity bits in code_word
-    for i , pos in enumerate( parity_positions ):
-        parity_seq: list[int] = []
+    for i,j in enumerate(parity_positions):
+        parity_sequence = []
         take = True
         take_count = 2**i
-        for j in range( pos , len(code_word) ):
+        for k in range(j,len(code_word)):
             if take:
-                parity_seq.append( code_word[j] )
+                parity_sequence.append(code_word[k])
                 take_count -= 1
             else:
                 take_count += 1
+            
             if take_count == 0:
                 take = False
             elif take_count == 2**i:
                 take = True
 
-        code_word[ pos ] = parity_func( parity_seq )
+        code_word[j] = parity(parity_sequence)
 
-    return code_word , r
+    return code_word,redundant_bits
 
-def decode( code_word: list[int] , r: int , parity: str ) -> dict[ str , int ]:
-    parity_func = get_even_parity if parity == "even" else get_odd_parity
+def reciever(code_word,p,r):
+    parity = even_parity if p == 'even' else odd_parity
+    parity_positions = [2**i - 1 for i in range(r)]
+    parity_bits = []
 
-    parity_positions = [ 2**i - 1 for i in range(r) ]
-    parity_bits: list[int] = []
-    for i , pos in enumerate( parity_positions ):
-        parity_seq: list[int] = []
+    for i,j in enumerate(parity_positions):
+        parity_sequence = []
         take = True
         take_count = 2**i
-        for j in range( pos , len(code_word) ):
+        for k in range(j,len(code_word)):
             if take:
-                parity_seq.append( code_word[j] )
+                parity_sequence.append(code_word[k])
                 take_count -= 1
             else:
                 take_count += 1
+            
             if take_count == 0:
                 take = False
             elif take_count == 2**i:
                 take = True
-        parity_bits.append( parity_func( parity_seq ) )
+
+        parity_bits.append(parity(parity_sequence))
 
     return parity_bits
-    
-parity = input( "Which parity do we need? ([even],odd) => " )
-parity = "even" if parity == "" else "odd" 
 
-while True:
+p = input("Press e for even parity and o for odd parity: ")
+p = 'even' if p == 'e' else 'odd'
+print( "---------- Transmitter -----------" )
+input_message_str = input( "Enter message: " )
+input_chars = list( input_message_str )
+input_chars_ascii = [ ord( c ) for c in input_chars ]
+bin_repr_lists = []
+print( "Data on transmitter side is -> " )
+for ( char , ascii ) in zip( input_chars , input_chars_ascii ):
+    bin_repr = dec_to_bin( ascii )
+    bin_repr_lists.append( bin_repr )
+    print( f"{char} -> {ascii} -> { ''.join([str(i) for i in bin_repr]) }" )
+code_word_lists = []
+num_r_bits = None
+print( "Code words on transmitter side ->" ) 
+for ( char , bin_repr ) in zip( input_chars , bin_repr_lists ):
+    code_word , num_r_bits = sender( bin_repr , p )
+    code_word_lists.append( code_word )
+    print( f"{char} -> { ''.join([str(i) for i in code_word]) }" )
+print( "Number of redundant bits => r = " , num_r_bits )
+print( "Number of bits in message => m = " , len( code_word_lists[0] ) - num_r_bits )
 
-    print( "---------- Transmitter -----------" )
-
-    input_message_str = input( "Enter message: " )
-    input_chars = list( input_message_str )
-    input_chars_ascii = [ ord( c ) for c in input_chars ]
-
-    bin_repr_lists = []
-    print( "Data on transmitter side is -> " )
-    for ( char , ascii ) in zip( input_chars , input_chars_ascii ):
-        bin_repr = decimal_to_binary( ascii )
-        bin_repr_lists.append( bin_repr )
-        print( f"{char} -> {ascii} -> { list_to_str(bin_repr) }" )
-
-    code_word_lists = []
-    num_r_bits = None
-    print( "Code words on transmitter side ->" ) 
-    for ( char , bin_repr ) in zip( input_chars , bin_repr_lists ):
-        code_word , num_r_bits = encode( bin_repr , parity )
-        code_word_lists.append( code_word )
-        print( f"{char} -> { list_to_str(code_word) }" )
-
-    print( "Number of redundant bits => r = " , num_r_bits )
-    print( "Number of bits in message => m = " , len( code_word_lists[0] ) - num_r_bits )
-
-    print( "---------- Channel / Transmission Medium -----------" )
-
-    error = input( "Do you wish to induce errors in the message? ([no],yes) => " ) 
-    if error == "yes":
-        for code_word in code_word_lists:
-            random_position = random.randint( 0 , len( code_word ) - 1 ) 
-            code_word[ random_position ] = 0 if code_word[random_position] == 1 else 1
-            print( "For code word {}, error induced at position={}".format( list_to_str(code_word) , random_position ) )
-
-    print( "---------- Receiver -----------" )
-
-    decoded_message: str = ""
+print( "---------- Channel / Transmission Medium -----------" )
+error = input( "Press y to enduce error else press n: " ) 
+if error == "yes":
     for code_word in code_word_lists:
-        code_word_str = list_to_str( code_word )
-        parity_bits = decode( code_word , num_r_bits , parity="even" )
+        random_position = random.randint( 0 , len( code_word ) - 1 ) 
+        code_word[ random_position ] = 0 if code_word[random_position] == 1 else 1
+        print( "For code word {}, error induced at position={}".format( ''.join([str(i) for i in code_word]) , random_position ) )
 
-        if sum( parity_bits ) == 0:
-            parity_positions = [ 2**i - 1 for i in range(num_r_bits) ]
-            char_bin_repr = [ c for (pos , c) in enumerate(code_word) if pos not in parity_positions ]
-            char = chr(binary_to_decimal( char_bin_repr )) 
-            print( f"Code {code_word_str} received without any error." , 
-                  f"Decoded character => { char }" )
-        else:
-            error_position = binary_to_decimal( parity_bits[::-1] ) - 1
-            print( f"Code {code_word_str} received with any error." , 
-                  f"Error is at index={ error_position }"
-                   )
-            code_word[error_position] = 0 if code_word[error_position] == 1 else 1
-            parity_positions = [ 2**i - 1 for i in range(num_r_bits) ]
-            char_bin_repr = [ c for (pos , c) in enumerate(code_word) if pos not in parity_positions ]
-            char = chr(binary_to_decimal( char_bin_repr )) 
-            print( f"Decoded character after correcting error => {char}" )
-        decoded_message += char
+print( "---------- Receiver -----------" )
+decoded_message: str = ""
+for code_word in code_word_lists:
+    code_word_str = ''.join([str(i) for i in code_word])
+    parity_bits = reciever( code_word , 'even', num_r_bits )
+    if sum( parity_bits ) == 0:
+        parity_positions = [ 2**i - 1 for i in range(num_r_bits) ]
+        char_bin_repr = [ c for (pos , c) in enumerate(code_word) if pos not in parity_positions ]
+        char = chr(bin_to_dec( char_bin_repr )) 
+        print( f"Code {code_word_str} received without any error." , 
+              f"Decoded character => { char }" )
+    else:
+        error_position = bin_to_dec( parity_bits[::-1] ) - 1
+        print( f"Code {code_word_str} received with any error." , 
+              f"Error is at index={ error_position }"
+               )
+        code_word[error_position] = 0 if code_word[error_position] == 1 else 1
+        parity_positions = [ 2**i - 1 for i in range(num_r_bits) ]
+        char_bin_repr = [ c for (pos , c) in enumerate(code_word) if pos not in parity_positions ]
+        char = chr(bin_to_dec( char_bin_repr )) 
+        print( f"Decoded character after correcting error => {char}" )
+    decoded_message += char
+
+print( "Message on receiver side =>" , decoded_message )
+
     
-    print( "Message on receiver side =>" , decoded_message )
+    
+
+
+
+
+
+
+
+
