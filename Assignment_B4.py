81e505ccb3c5e0b28e431859e5d4afe3aea82d5c
Advait Naik <ad.naik0801@gmail.com>
2023-11-17 22:21:12 +0530

    Page Replacement Algorithms completed

--- Assignment_B4.py
+++ Assignment_B4.py
@@ -5,35 +5,31 @@ class PageReplacementAlgorithms:
 
     def first_in_first_out(self):
         frames = []
-        page_fault_count = 0
+        miss_count = 0
 
         for i in range(len(self.page_numbers)):
-            print(frames, end=' '.ljust(10))
-
             if self.page_numbers[i] not in frames:
-                print(str(self.page_numbers[i]).ljust(10)," Miss ".ljust(10))
-                page_fault_count += 1
+                miss_count += 1
                 if len(frames) == self.max_num_frames:
                     frames.pop(0)
                 frames.append(self.page_numbers[i])
+                print(str(self.page_numbers[i]).ljust(5) + " Miss ".ljust(5) + str(frames))
             else:
-                print(str(self.page_numbers[i]).ljust(10)," Hit ".ljust(10))
+                print(str(self.page_numbers[i]).ljust(5) + " Hit ".ljust(5) + str(frames))
             
-        return page_fault_count / len(self.page_numbers)
+        return miss_count, len(self.page_numbers) - miss_count
     
     def least_recently_used(self):
         frames , frame_ages = [], []
-        page_fault_count = 0
+        miss_count = 0
 
-        for i in range(len(self.page_numbers)):
-            print(frames, end=' '.ljust(10))  
+        for i in range(len(self.page_numbers)): 
 
             for j in range(len(frame_ages)):
                 frame_ages[j] += 1
 
             if self.page_numbers[i] not in frames:
-                print(str(self.page_numbers[i]).ljust(10)," Miss ".ljust(10))
-                page_fault_count += 1
+                miss_count += 1
                 if len(frames) == self.max_num_frames:
                     max_index = frame_ages.index(max(frame_ages))
                     frame_ages.pop(max_index)
@@ -41,25 +37,24 @@ class PageReplacementAlgorithms:
                     frames.insert(max_index, self.page_numbers[i])
                     frame_ages.insert(max_index, 0)
                 else:
-                    frames.insert(0, self.page_numbers[i])
-                    frame_ages.insert(0, 0)
+                    frames.append( self.page_numbers[i])
+                    frame_ages.append(0)
+                print(str(self.page_numbers[i]).ljust(5) + " Miss ".ljust(5) + str(frames))
             else:
-                print(str(self.page_numbers[i]).ljust(10)," Hit ".ljust(10))
                 index = frames.index(self.page_numbers[i])
                 frame_ages[index] = 0
+                print(str(self.page_numbers[i]).ljust(5) + " Hit ".ljust(5) + str(frames))
 
-        return page_fault_count / len(self.page_numbers)
+        return miss_count, len(self.page_numbers) - miss_count
     
     def optimal_approach(self):
         frames = []
-        page_fault_count = 0
+        miss_count = 0
 
         for i in range(len(self.page_numbers)):
-            print(frames, end=' '.ljust(10))
 
             if self.page_numbers[i] not in frames:
-                print(str(self.page_numbers[i]).ljust(10)," Miss ".ljust(10))
-                page_fault_count += 1
+                miss_count += 1
 
                 if len(frames) == self.max_num_frames:
                     max_usage_gap = -1
@@ -82,28 +77,30 @@ class PageReplacementAlgorithms:
                     frames[max_usage_gap_index] = self.page_numbers[i]
                 else:
                     frames.append(self.page_numbers[i])
+                print(str(self.page_numbers[i]).ljust(5) + " Miss ".ljust(5) + str(frames))
             else:
-                print(str(self.page_numbers[i]).ljust(10)," Hit ".ljust(10))
+                print(str(self.page_numbers[i]).ljust(5) + " Hit ".ljust(5) + str(frames))
                 
-        return page_fault_count / len(self.page_numbers)
+        return miss_count, len(self.page_numbers) - miss_count
     
 
 
 frames = [7,0,1,2,0,3,0,4,2,3,0,3,2,3]
 algo = PageReplacementAlgorithms(frames, 4)
-miss_ratio = 0
+frame_miss_count = 0
+frame_hit_count = 0
 
 print("FIFO.....")
-miss_ratio = algo.first_in_first_out()
-print("Miss ratio: ",  miss_ratio)
+frame_miss_count, frame_hit_count = algo.first_in_first_out()
+print("Hit count: " + str(frame_hit_count) + " Miss count: " + str(frame_miss_count))
 print()
 
 print("LRU.......")
-miss_ratio = algo.least_recently_used()
-print("Miss ratio: ",  miss_ratio)
+frame_miss_count, frame_hit_count = algo.least_recently_used()
+print("Hit count: " + str(frame_hit_count) + " Miss count: " + str(frame_miss_count))
 print()
 
 print("Optimal......")
-miss_ratio = algo.optimal_approach()
-print("Miss ratio: ",  miss_ratio)
+frame_miss_count, frame_hit_count = algo.optimal_approach()
+print("Hit count: " + str(frame_hit_count) + " Miss count: " + str(frame_miss_count))
 print()
