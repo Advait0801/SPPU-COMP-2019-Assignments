59c49a0cac84d602cda5fd770950b68837565ff4
Advait Naik <ad.naik0801@gmail.com>
2023-11-19 12:04:28 +0530

    Scheduling algorithms in python completed

--- Assignment_B1/scheduling.py
+++ Assignment_B1/scheduling.py
@@ -1,254 +1,207 @@
 class Process:
-    def __init__(self, name) -> None:
-        self.name = name
+    def __init__(self) -> None:
+        self.id = 0
         self.priority = 0
-        self.arrival_time = 0
-        self.burst_time = 0
-        self.completion_time = 0
-        self.turn_around_time = 0
-        self.wait_time = 0
-        self.response_time = 0
+        self.at = 0 #arrival time
+        self.bt = 0 #burst time
+        self.ct = 0 #completion time
+        self.tat = 0 #turn_around time
+        self.wt = 0 #wait time
+        self.rt = 0 #response time
+
+    def calculate_times(self):
+        self.tat = self.ct - self.at
+        self.wt = self.tat - self.bt
 
     def display(self):
-        print("Process " + str(self.name) + " priority: " + str(self.priority) + " AT: " + str(self.arrival_time) + " BT: " + str(self.burst_time) + " CT: " + str(self.completion_time) + " TAT: " + str(self.turn_around_time) + " WT: " + str(self.wait_time) + " RT: " + str(self.response_time))
-    
+        print("Process" + str(self.id).ljust(3) + " priority: " + str(self.priority).ljust(3) + " AT: " + str(self.at).ljust(3) + " BT: " + str(self.bt).ljust(3) + " CT: " + str(self.ct).ljust(3) + " TAT: " + str(self.tat).ljust(3) + " WT: " + str(self.wt).ljust(3) + " RT: " + str(self.rt))
+
 
 class SchedulingAlgorithms:
     def __init__(self, processes: list[Process]) -> None:
-        self.processes = processes
-        self.tat = 0
-        self.wt = 0
+        self.processes = processes    
 
-    def first_come_first_serve(self):
-        self.wt = 0
-        self.tat = 0
+    def fifo(self):
         ready_queue: list[Process] = []
-        output: list[Process] = []
-
+        ans: list[Process] = []
         time = 0
+        current_process: Process = None
+        process_count = 0
         is_executing = False
-        current_process = self.processes[0]
-        n = 0
 
-        while n < len(self.processes):
-            for process in self.processes:
-                if process.arrival_time == time:
-                    ready_queue.append(process)
+        while process_count < len(self.processes):
+            for p in self.processes:
+                if p.at == time:
+                    ready_queue.append(p)
 
-            if is_executing == True and time == current_process.response_time + current_process.burst_time:
+            if is_executing == True and time == current_process.rt + current_process.bt:
+                current_process.ct = time
+                current_process.rt -= current_process.at
+                current_process.calculate_times()
+                ans.append(current_process)
+                process_count += 1
                 is_executing = False
-                current_process.completion_time = time
-                current_process.turn_around_time = current_process.completion_time - current_process.arrival_time
-                current_process.wait_time = current_process.turn_around_time - current_process.burst_time
-                output.append(current_process)
-                n += 1
 
-            if len(ready_queue) != 0 and is_executing == False:
-                current_process = ready_queue[0]
-                ready_queue.pop(0)
-                current_process.response_time = time
+            if not is_executing and len(ready_queue) > 0:
+                current_process = ready_queue.pop(0)
+                current_process.rt = time
                 is_executing = True
-            
+
             time += 1
-        
 
-        for process in output:
-            self.tat += process.turn_around_time
-            self.wt += process.wait_time
-            process.display()
+        avg_tat = sum([proc.tat for proc in ans]) / len(self.processes)
+        avg_wt = sum([proc.wt for proc in ans]) / len(self.processes)
+        for p in ans:
+            p.display()
+        print("Average TAT: " + str(avg_tat))
+        print("Average WT: " + str(avg_wt))
+        print()
 
-        print("Average TAT: ", self.tat / len(output))
-        print("Average WT: ", self.wt / len(output))
-
-    def round_robin(self, quantum):
-        self.wt = 0
-        self.tat = 0
+    def srtf(self):
         burst_times = {}
-        for process in self.processes:
-            burst_times[process.name] = process.burst_time
+        for p in self.processes:
+            burst_times[p.id] = p.bt
 
-        ready_queue: list[Process]  = []
-        output: list[Process] = []
-        current_process = self.processes[0]
-
+        ready_queue: list[Process] = []
+        ans: list[Process] = []
         time = 0
+        process_count = 0
+        current_process: Process = None
+        current_process_remaining_bt = 1e+4
         is_executing = False
-        n = 0
-        step = 0
-        current_process_remaining_bt = 1000
 
-        while n < len(self.processes):
-            for process in self.processes:
-                if process.arrival_time == time:
-                    ready_queue.append(process)
+        while process_count < len(self.processes):
+            for p in self.processes:
+                if p.at == time:
+                    ready_queue.append(p)
 
-            if current_process_remaining_bt == 0:
-                current_process.response_time -= current_process.arrival_time
-                current_process.burst_time = burst_times[process.name]
-                current_process.completion_time = time
-                current_process.turn_around_time = current_process.completion_time - current_process.arrival_time
-                current_process.wait_time = current_process.turn_around_time - current_process.burst_time
-                output.append(current_process)
+            if is_executing == True and current_process_remaining_bt == 0:
+                current_process.ct = time
+                current_process.bt = burst_times[current_process.id]
+                current_process.calculate_times()
+                ans.append(current_process)
+                process_count += 1
                 is_executing = False
-                n += 1
 
-                if len(ready_queue) != 0:
-                    current_process = ready_queue[0]
-                    if current_process.response_time == 0:
-                        current_process.response_time = time
-                    current_process_remaining_bt = current_process.burst_time
-                    step = time + quantum
-                    is_executing = True
-                    ready_queue.pop(0)
-
-            if is_executing == True:
-                if time == step:
-                    current_process.burst_time = current_process_remaining_bt
+            if is_executing == True and len(ready_queue) > 0:
+                min_bt_process = min(ready_queue, key=lambda p:p.bt)
+                if min_bt_process.bt < current_process.bt:
+                    current_process.bt = current_process_remaining_bt
                     ready_queue.append(current_process)
+                    current_process = min_bt_process
+                    current_process_remaining_bt = current_process.bt
+                    ready_queue.remove(min_bt_process)
 
-                    if len(ready_queue) != 0:
-                        current_process = ready_queue[0]
-                        if current_process.response_time == 0:
-                            current_process.response_time = time
-                        current_process_remaining_bt = current_process.burst_time
-                        step = time + quantum
-                        is_executing = True
-                        ready_queue.pop(0)
-            else:
-                if len(ready_queue) != 0:
-                    current_process = ready_queue[0]
-                    current_process_remaining_bt = current_process.burst_time
-                    step = time + quantum
-                    is_executing = True
-                    ready_queue.pop(0)
+            if not is_executing and len(ready_queue) > 0:
+                min_bt_process = min(ready_queue, key=lambda p:p.bt)
+                ready_queue.remove(min_bt_process)
+                current_process = min_bt_process
+                current_process.rt = time
+                current_process_remaining_bt = current_process.bt
+                is_executing = True
 
             if is_executing == True:
                 current_process_remaining_bt -= 1
-            
+
             time += 1
 
-        for process in output:
-            self.tat += process.turn_around_time
-            self.wt += process.wait_time
-            process.display()
-
-        print("Average TAT: ", self.tat / len(output))
-        print("Average WT: ", self.wt / len(output))
+        avg_tat = sum([proc.tat for proc in ans]) / len(self.processes)
+        avg_wt = sum([proc.wt for proc in ans]) / len(self.processes)
+        for p in ans:
+            p.display()
+        print("Average TAT: " + str(avg_tat))
+        print("Average WT: " + str(avg_wt))
+        print()
 
     def priority(self):
-        self.wt = 0
-        self.tat = 0
         ready_queue: list[Process] = []
-        output: list[Process] = []
-
+        ans: list[Process] = []
         time = 0
+        process_count = 0
+        current_process: Process = None
         is_executing = False
-        current_process = self.processes[0]
-        n = 0
 
-        while n < len(self.processes):
-            for process in self.processes:
-                if process.arrival_time == time:
-                    ready_queue.append(process)
+        while process_count < len(self.processes):
+            for p in self.processes:
+                if p.at == time:
+                    ready_queue.append(p)
 
-            if is_executing == True and time == current_process.response_time + current_process.burst_time:
+            if is_executing == True and time == current_process.rt + current_process.bt:
+                current_process.rt -= current_process.at
+                current_process.ct = time
+                current_process.calculate_times()
+                process_count += 1
                 is_executing = False
-                current_process.completion_time = time
-                current_process.turn_around_time = current_process.completion_time - current_process.arrival_time
-                current_process.wait_time = current_process.turn_around_time - current_process.burst_time
-                output.append(current_process)
-                n += 1
+                ans.append(current_process)
 
-            if len(ready_queue) != 0 and is_executing == False:
-                max_priority = 999
-                process_index = 0
-                for i in range(len(ready_queue)):
-                    if ready_queue[i].priority < max_priority:
-                        max_priority = ready_queue[i].priority
-                        current_process = ready_queue[i]
-                        process_index = i
-                current_process.response_time = time
-                ready_queue.pop(process_index)
+            if not is_executing and len(ready_queue) > 0:
+                current_process = min(ready_queue, key=lambda p:p.priority)
+                ready_queue.remove(current_process)
+                current_process.rt = time
                 is_executing = True
+
             time += 1
 
-        for process in output:
-            self.tat += process.turn_around_time
-            self.wt += process.wait_time
-            process.display()
+        avg_tat = sum([proc.tat for proc in ans]) / len(self.processes)
+        avg_wt = sum([proc.wt for proc in ans]) / len(self.processes)
+        for p in ans:
+            p.display()
+        print("Average TAT: " + str(avg_tat))
+        print("Average WT: " + str(avg_wt))
+        print()
 
-        print("Average TAT: ", self.tat / len(output))
-        print("Average WT: ", self.wt / len(output))
-
-    def smallest_job_first(self):
-        self.wt = 0
-        self.tat = 0
+    def round_robin(self, quantum):
         burst_times = {}
-        for process in self.processes:
-            burst_times[process.name] = process.burst_time
+        for p in self.processes:
+            burst_times[p.id] = p.bt
 
-        ready_queue: list[Process]  = []
-        output: list[Process] = []
-        current_process = self.processes[0]
-
+        ready_queue: list[Process] = []
+        ans: list[Process] = []
         time = 0
+        process_count = 0
+        current_process: Process = None
         is_executing = False
-        n = 0
-        current_process_remaining_bt = 1000
+        current_process_remaining_bt = None
+        step = None
 
-        while n < len(self.processes):
-            for process in self.processes:
-                if process.arrival_time == time:
-                    ready_queue.append(process)
+        while process_count < len(self.processes):
+            for p in self.processes:
+                if p.at == time:
+                    ready_queue.append(p)
 
-            if current_process_remaining_bt == 0:
-                current_process.response_time -= current_process.arrival_time
-                current_process.burst_time = burst_times[current_process.name]
-                current_process.completion_time = time
-                current_process.turn_around_time = current_process.completion_time - current_process.arrival_time
-                current_process.wait_time = current_process.turn_around_time - current_process.burst_time
-                output.append(current_process)
-                n += 1
+            if is_executing == True and current_process_remaining_bt == 0:
+                current_process.ct = time
+                current_process_remaining_bt = burst_times[current_process.id]
+                current_process.calculate_times()
+                process_count += 1
+                ans.append(current_process)
                 is_executing = False
 
-                if len(ready_queue) != 0:
-                    ready_queue = sorted(ready_queue, key=lambda p: p.burst_time)
-                    if ready_queue[0].response_time == 0:
-                        ready_queue[0].response_time = time
-                    current_process_remaining_bt = ready_queue[0].burst_time
-                    current_process = ready_queue[0]
-                    ready_queue.pop(0)
-                    is_executing = True
-            else:
-                if len(ready_queue) != 0:
-                    ready_queue = sorted(ready_queue, key=lambda p: p.burst_time)
-                    if is_executing == True:
-                        if ready_queue[0].burst_time < current_process_remaining_bt:
-                            current_process.burst_time = current_process_remaining_bt
-                            ready_queue.append(current_process)
-                            current_process = ready_queue[0]
-                            ready_queue.pop(0)
-                            current_process_remaining_bt = current_process.burst_time
-                            is_executing = True
-                    else:
-                        current_process = ready_queue[0]
-                        ready_queue.pop(0)
-                        current_process_remaining_bt = current_process.burst_time
-                        is_executing = True
+            if is_executing == True and time == step:
+                current_process.bt = current_process_remaining_bt
+                ready_queue.append(current_process)
+                is_executing = False
+
+            if not is_executing and len(ready_queue) > 0:
+                current_process = ready_queue.pop(0)
+                current_process.rt = time
+                current_process_remaining_bt = current_process.bt
+                step = time + quantum
+                is_executing = True
 
             if is_executing == True:
                 current_process_remaining_bt -= 1
+
             time += 1
 
-        for process in output:
-            self.tat += process.turn_around_time
-            self.wt += process.wait_time
-            process.display()
+        avg_tat = sum([proc.tat for proc in ans]) / len(self.processes)
+        avg_wt = sum([proc.wt for proc in ans]) / len(self.processes)
+        for p in ans:
+            p.display()
+        print("Average TAT: " + str(avg_tat))
+        print("Average WT: " + str(avg_wt))
+        print()
 
-        print("Average TAT: ", self.tat / len(output))
-        print("Average WT: ", self.wt / len(output))
-
-        
 
 burst_times = [6,2,1,9,8]
 arrival_times = [0,2,3,4,5]
@@ -256,26 +209,30 @@ priorities = [3,1,4,5,2]
 
 processes: list[Process] = []
 for i in range(5):
-    name = "P" + str(i+1)
-    p = Process(name)
-    p.burst_time = burst_times[i]
-    p.arrival_time = arrival_times[i]
+    p = Process()
+    p.id = i+1
+    p.bt = burst_times[i]
+    p.at = arrival_times[i]
     p.priority = priorities[i]
     processes.append(p)
 
 obj = SchedulingAlgorithms(processes=processes)
 print("FCFS......")
-obj.first_come_first_serve()
+obj.fifo()
 print()
 
-# print("Priority....")
-# obj.priority()
-# print()
+print("Priority....")
+obj.priority()
+print()
 
-# print("SJF......")
-# obj.smallest_job_first()
-# print()
+print("SJF......")
+obj.srtf()
+print()
 
-# print("Round Robin......")
-# obj.round_robin(2)
-# print()
+print("Round Robin......")
+obj.round_robin(2)
+print()
+
+        
+
+                
