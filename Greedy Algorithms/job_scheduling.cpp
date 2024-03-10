5c25f62db1eeb01fe975f4415be429a9896cefa9
Advait Naik <ad.naik0801@gmail.com>
2024-03-10 22:54:43 +0530

    Job Scheduling completed

--- Greedy Algorithms/job_scheduling.cpp
+++ Greedy Algorithms/job_scheduling.cpp
@@ -32,37 +32,51 @@ int jobScheduling(vector<Job> &jobs){
             jobs[j] = jobs[j-1];
             jobs[j-1] = temp;
             j--;
-        }
+        }     
+    }
 
-        vector<int> deadlines(n, 0);
-        for(j=0;j<n;j++){
-            deadlines[j] = jobs[j].deadline;
-        }
-        int maxDeadline = *max_element(deadlines.begin(), deadlines.end());
+    int maxDeadline = INT_MIN;
+    for(int i=0;i<n;i++){
+        maxDeadline = max(maxDeadline, jobs[i].deadline);
+    }
 
-        vector<bool> slotStatus(maxDeadline, true);
-        vector<Job> maxProfitSeq;
+    vector<bool> slotStatus(maxDeadline, true);
+    vector<Job> maxProfitSequence;
 
-        for(j=0;j<n;j++){
-            Job job = jobs[j];
+    for(int i=0;i<n;i++){
+        Job job = jobs[i];
 
-            for(int k=job.deadline;k>=0;k--){
-                if(slotStatus[k] == true){
-                    slotStatus[k] = false;
-                    maxProfitSeq.push_back(job);
-                    break;
-                }
+        for(int j=job.deadline-1;j>=0;j--){
+            if(slotStatus[j] == true){
+                slotStatus[j] = false;
+                maxProfitSequence.push_back(job);
+                break;
             }
         }
-
-        int m = maxProfitSeq.size();
-        int maxProfit = 0;
-        for(j=0;j<m;j++){
-            maxProfitSeq[j].print();
-            maxProfit += maxProfitSeq[j].profit;
-        }
-
-        return maxProfit;      
-        
     }
+
+    int maxProfit = 0;
+    int m = maxProfitSequence.size();
+    for(int i=0;i<m;i++){
+        maxProfitSequence[i].print();
+        maxProfit += maxProfitSequence[i].profit;
+    }
+
+    return maxProfit;
+}
+
+
+int main(){
+    vector<Job> jobs = {
+        Job(1, 20, 2),
+        Job(2, 15, 2),
+        Job(3, 10, 1),
+        Job(4, 5, 3),
+        Job(5, 1, 3),
+    };
+
+    int ans = jobScheduling(jobs);
+    cout<<"Max Profit = "<<ans<<endl;
+
+    return 0;
 }
