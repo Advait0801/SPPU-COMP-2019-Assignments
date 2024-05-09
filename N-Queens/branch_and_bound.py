55e14f5cb631448d941c1dc60a62aa8c49f8250b
Advait Naik <ad.naik0801@gmail.com>
2024-05-09 09:27:46 +0530

    Branch and Bound fixed for N-Queens

--- N-Queens/branch_and_bound.py
+++ N-Queens/branch_and_bound.py
@@ -1,53 +1,59 @@
+from heapq import heappush, heappop
+import copy
+
 def is_safe(board, row, col):
-    # Check if there is any queen in the same column
     for i in range(row):
-        if board[i] == col:
+        if board[i] == col or abs(board[i] - col) == abs(i - row):
             return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i] == j:
-            return False
-
-    # Check upper diagonal on right side
-    for i, j in zip(range(row, -1, -1), range(col, len(board))):
-        if board[i] == j:
-            return False
-
+    
     return True
 
-def solve_n_queens(board, row, n, solutions):
-    if row == n:
-        solutions.append(list(board))
-        return
-
-    for col in range(n):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_n_queens(board, row + 1, n, solutions)
-            board[row] = -1
-
-def n_queens_branch_and_bound(n):
-    board = [-1] * n
-    solutions = []
-    solve_n_queens(board, 0, n, solutions)
-    
-    return solutions
-
-def print_board(board):
-    n = len(board)
+def objective_func(board, n):
+    cost = 0
     for i in range(n):
-        for j in range(n):
-            if board[i] == j:
-                print("Q ", end="")
-            else:
-                print(". ", end="")
-        print()
-    print()
+        for j in range(i + 1, n):
+            if board[i] == board[j] or abs(board[j] - board[i]) == j - 1:
+                cost += 1
+    
+    return cost
 
+def solve(n):
+    board = [-1] * n
+    queue = []
+    heappush(queue, (objective_func(board, n), board))
+    flag = False
 
-n = 4
-solutions = n_queens_branch_and_bound(n)
-print("Number of solutions for", n, "-queens:", len(solutions))
-for solution in solutions:
-    print_board(solution)
+    while len(queue) != 0:
+        curr_node = heappop(queue)
+        curr_board = curr_node[1]
+
+        row = 0
+        while row < n and curr_board[row] != -1:
+            row += 1
+
+        if row == n:
+            print("The final positions of queens are as follows...")
+            for i in range(n):
+                for j in range(n):
+                    if curr_board[i] == j:
+                        print('Q ', end = ' ')
+                    else:
+                        print('. ', end = ' ')
+                print()
+            print()
+            flag = True
+            return
+        
+        for col in range(n):
+            if is_safe(curr_board, row, col):
+                new_board = copy.deepcopy(curr_board)
+                new_board[row] = col
+                print(new_board)
+                new_cost = objective_func(new_board, n)
+                heappush(queue, (new_cost, new_board))
+
+    if not flag:
+        print("Solution doesnt exist for n = " + str(n))
+
+n = int(input("Enter no of queens: "))
+solve(n)    
