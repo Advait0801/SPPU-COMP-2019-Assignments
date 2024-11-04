26877c683d96189bda4e8677d5418acde07caaf0
Advait Naik <ad.naik0801@gmail.com>
2024-11-04 23:30:44 +0530

    0/1 Knapsack DP and BNB updated

--- Zero-1 KnapSack/knapsack_bnb.cpp
+++ Zero-1 KnapSack/knapsack_bnb.cpp
@@ -23,21 +23,8 @@ struct Node {
     Node(int l, int v, int w) : level(l), value(v), weight(w), bound(0.0) {}
 };
 
-class KnapSack {
-    int solve(int i, int capacity, vector<int> &weights, vector<int> &values, vector<vector<int>> &dp) {
-        if(i == 0) {
-            if(weights[0] <= capacity) return values[0];
-            return 0;
-        }
-        if(dp[i][capacity] != -1) return dp[i][capacity];
 
-        int notTaken = solve(i-1, capacity, weights, values, dp);
-        int taken = INT_MIN;
-        if(weights[i] <= capacity) taken = values[i] + solve(i-1, capacity - weights[i], weights, values, dp);
-
-        return dp[i][capacity] = max(taken, notTaken);
-    }
-
+class Knapsack {
     double calculateBound(Node *node, int capacity, vector<Item> &items) {
         // If we've exceeded the capacity, this node is not valid
         if (node -> weight >= capacity) return 0;
@@ -63,54 +50,6 @@ class KnapSack {
     }
 
 public:
-    void memoization(int maxCapacity, vector<int> &weights, vector<int> &values) {
-        int n = weights.size();
-        vector<vector<int>> dp(n, vector<int> (maxCapacity + 1, -1));
-        int maxProfit = solve(n-1, maxCapacity, weights, values, dp);
-
-        cout<<"The max profit by recursion-memoization is "<<maxProfit<<endl;  
-    }
-
-    void tabulation(int maxCapacity, vector<int> &weights, vector<int> &values) {
-        int n = weights.size();
-        vector<vector<int>> dp(n, vector<int> (maxCapacity + 1, 0));
-        for(int w=weights[0];w<=maxCapacity;w++) {
-            dp[0][w] = values[0];
-        }
-
-        for(int i=1;i<n;i++) {
-            for(int w=0;w<=maxCapacity;w++) {
-                int notTaken = dp[i-1][w];
-                int taken = INT_MIN;
-                if(weights[i] <= w) taken = values[i] + dp[i-1][w - weights[i]];
-                dp[i][w] = max(taken, notTaken); 
-            }
-        }
-
-        cout<<"The max profit by tabulation is "<<dp[n-1][maxCapacity]<<endl;
-    }
-
-    void spaceOptimized(int maxCapacity, vector<int> &weights, vector<int> &values) {
-        int n = weights.size();
-        vector<int> prev(maxCapacity + 1, 0);
-        for(int w=weights[0];w<=maxCapacity;w++) {
-            prev[w] = values[0];
-        }
-
-        for(int i=1;i<n;i++) {
-            vector<int> curr(maxCapacity + 1, 0);
-            for(int w=0;w<=maxCapacity;w++) {
-                int notTaken = prev[w];
-                int taken = INT_MIN;
-                if(weights[i] <= w) taken = values[i] + prev[w - weights[i]];
-                curr[w] = max(taken, notTaken); 
-            }
-            prev = curr;
-        }
-
-        cout<<"The max profit by space optimization is "<<prev[maxCapacity]<<endl;
-    }
-
     void branchAndBound(int maxCapacity, vector<int> &weights, vector<int> &values) {
         int n = weights.size();    
         // Create and sort items by value/weight ratio
@@ -203,15 +142,11 @@ public:
 };
 
 int main() {
-    vector<int> weights = {95, 4, 60, 32, 23, 72, 80, 62, 65, 46};
-    vector<int> values = {55, 10, 47, 5, 4, 50, 8, 61, 85, 87};
-    int capacity = 269;
+    vector<int> weights = {3, 4, 6, 5};
+    vector<int> values = {2, 3, 1, 4};
 
-    KnapSack k;
-    k.memoization(capacity, weights, values);
-    k.tabulation(capacity, weights, values);
-    k.spaceOptimized(capacity, weights, values);
-    k.branchAndBound(capacity, weights, values);
-
+    Knapsack k;
+    k.branchAndBound(8, weights, values);
+    
     return 0;
 }
---
+++ Zero-1 KnapSack/knapsack_dp.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class KnapSack {
+    int solve(int i, int capacity, vector<int> &weights, vector<int> &values, vector<vector<int>> &dp) {
+        if(i == 0) {
+            if(weights[0] <= capacity) return values[0];
+            return 0;
+        }
+        if(dp[i][capacity] != -1) return dp[i][capacity];
+
+        int notTaken = solve(i-1, capacity, weights, values, dp);
+        int taken = INT_MIN;
+        if(weights[i] <= capacity) taken = values[i] + solve(i-1, capacity - weights[i], weights, values, dp);
+
+        return dp[i][capacity] = max(taken, notTaken);
+    }
+
+public:
+    void memoization(int maxCapacity, vector<int> &weights, vector<int> &values) {
+        int n = weights.size();
+        vector<vector<int>> dp(n, vector<int> (maxCapacity + 1, -1));
+        int maxProfit = solve(n-1, maxCapacity, weights, values, dp);
+
+        cout<<"The max profit by recursion-memoization is "<<maxProfit<<endl;  
+    }
+
+    void tabulation(int maxCapacity, vector<int> &weights, vector<int> &values) {
+        int n = weights.size();
+        vector<vector<int>> dp(n, vector<int> (maxCapacity + 1, 0));
+        for(int w=weights[0];w<=maxCapacity;w++) {
+            dp[0][w] = values[0];
+        }
+
+        for(int i=1;i<n;i++) {
+            for(int w=0;w<=maxCapacity;w++) {
+                int notTaken = dp[i-1][w];
+                int taken = INT_MIN;
+                if(weights[i] <= w) taken = values[i] + dp[i-1][w - weights[i]];
+                dp[i][w] = max(taken, notTaken); 
+            }
+        }
+
+        cout<<"The max profit by tabulation is "<<dp[n-1][maxCapacity]<<endl;
+    }
+
+    void spaceOptimized(int maxCapacity, vector<int> &weights, vector<int> &values) {
+        int n = weights.size();
+        vector<int> prev(maxCapacity + 1, 0);
+        for(int w=weights[0];w<=maxCapacity;w++) {
+            prev[w] = values[0];
+        }
+
+        for(int i=1;i<n;i++) {
+            vector<int> curr(maxCapacity + 1, 0);
+            for(int w=0;w<=maxCapacity;w++) {
+                int notTaken = prev[w];
+                int taken = INT_MIN;
+                if(weights[i] <= w) taken = values[i] + prev[w - weights[i]];
+                curr[w] = max(taken, notTaken); 
+            }
+            prev = curr;
+        }
+
+        cout<<"The max profit by space optimization is "<<prev[maxCapacity]<<endl;
+    }
+};
+
+int main() {
+    vector<int> weights = {95, 4, 60, 32, 23, 72, 80, 62, 65, 46};
+    vector<int> values = {55, 10, 47, 5, 4, 50, 8, 61, 85, 87};
+    int capacity = 269;
+
+    KnapSack k;
+    k.memoization(capacity, weights, values);
+    k.tabulation(capacity, weights, values);
+    k.spaceOptimized(capacity, weights, values);
+
+    return 0;
+}
